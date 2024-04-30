6e51037f3c5789be48060da1f2b99ed531a2d9cc
kicre <kicre@gmx.com>
2024-04-30 12:20:12 +0800

    add window size

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -82,6 +82,8 @@ config.font_size = 12
 config.launch_menu = launch_menu
 config.default_cursor_style = 'SteadyUnderline'
 config.enable_tab_bar = false
+config.initial_cols = 96
+config.initial_rows = 28
 
 return config
 
