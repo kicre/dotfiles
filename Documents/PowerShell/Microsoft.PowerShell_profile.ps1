faa314fbd44b3113a3fd95261700ae186cc1dc20
kicre <kicre@gmx.com>
2025-04-24 12:10:15 +0800

    add ls aliases

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -29,3 +29,31 @@ function noproxy {
     $env:HTTPS_PROXY = ""
     Write-Host "Unset Proxy"
 }
+
+# Aliases
+# ls
+if (Get-Command eza -ErrorAction SilentlyContinue) {
+ Remove-Item Alias:ls -Force -ErrorAction SilentlyContinue
+    function ls { eza --icons --git @args }
+    function l { eza --icons --git @args }
+    function la { eza --icons --all --group-directories-first --git @args }
+    function ll { eza -l --icons --all --all --group-directories-first --git @args }
+    function lt { eza -T --icons --git-ignore --level=2 --group-directories-first @args }
+    function llt { eza -lT--icons  --git-ignore --level=2 --group-directories-first @args }
+    function lT { eza -T --icons --git-ignore --level=4 --group-directories-first @args }
+}
+elseif (Get-Command lsd -ErrorAction SilentlyContinue) {
+    Remove-Item Alias:ls -Force -ErrorAction SilentlyContinue
+    function ls { lsd @args }
+    function l { lsd @args }
+    function la { lsd --all --group-directories-first @args }
+    function ll { lsd -l --all --group-directories-first @args }
+    function lt { lsd --tree --depth=2 --group-directories-first @args }
+    function llt { lsd -l --tree --depth=2 --group-directories-first @args }
+    function lT { lsd --tree --depth=4 --group-directories-first @args }
+}
+else {
+    function l { Get-ChildItem -Force | Format-Table -AutoSize }
+    function ll { Get-ChildItem -Force -Detailed | Format-Table -AutoSize }
+    function la { Get-ChildItem -Force -Hidden | Format-Table -AutoSize }
+}
