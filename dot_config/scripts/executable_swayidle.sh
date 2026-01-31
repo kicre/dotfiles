e4c83228f0f488e3e869665380be2698d4b1045a
kicre <kicre@gmx.com>
2026-01-31 23:42:58 +0800

    fix hyprlock cmd

--- dot_config/scripts/executable_swayidle.sh
+++ dot_config/scripts/executable_swayidle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 swayidle -w \
-        timeout 300   'hyprlock -f' \
+        timeout 300   'hyprlock &' \
         timeout 600   'niri msg action power-off-monitors' \
         resume        'niri msg action power-on-monitors' \
         timeout 1200  'systemctl suspend'
