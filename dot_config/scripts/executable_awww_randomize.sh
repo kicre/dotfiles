d62b7fd1e3634e004d9453ea4d7130c45ba32678
kicre <kicre@gmx.com>
2026-02-01 19:29:23 +0800

    fix wallpaper

--- dot_config/scripts/executable_awww_randomize.sh
+++ dot_config/scripts/executable_awww_randomize.sh
@@ -3,8 +3,11 @@
 # at a set interval.
 
 DEFAULT_INTERVAL=300 # In seconds
+DEFAULT_WALLPAPER_DIR="${HOME}/Pictures/wallpapers"
 
-if [ $# -lt 1 ] || [ ! -d "$1" ]; then
+WALLPAPER_DIR="${1:-$DEFAULT_WALLPAPER_DIR}"
+
+if [ ! -d "$WALLPAPER_DIR" ]; then
 	printf "Usage:\n\t\e[1m%s\e[0m \e[4mDIRECTORY\e[0m [\e[4mINTERVAL\e[0m]\n" "$0"
 	printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every\n\tINTERVAL seconds (or every %d seconds if unspecified)." "$DEFAULT_INTERVAL"
 	exit 1
@@ -16,7 +19,7 @@ export AWWW_TRANSITION_FPS="${AWWW_TRANSITION_FPS:-60}"
 export AWWW_TRANSITION_STEP="${AWWW_TRANSITION_STEP:-2}"
 
 while true; do
-	find "$1" -type f \
+	find "$WALLPAPER_DIR" -type f \
 	| while read -r img; do
 		echo "$(</dev/urandom tr -dc a-zA-Z0-9 | head -c 8):$img"
 	done \
